d606f5f2d622539f88bc57fb562ab7f8b011d2f3
Julian Lam <julian.lam@gmail.com>
2013-08-22 11:43:17 -0400

    added check so that only existing user slugs get linkified

--- library.js
+++ library.js
@@ -1,14 +1,31 @@
-var	Mentions = {
-		addMentions: function(postContent, callback) {
-			// '@baris is wurst, @julianlam is best'.replace(/(@\b[\w\d\-_]+\b)/g, '<a href="#">$1</a>');
-			var	regex = /(@\b[\w\d\-_]+\b)/g,
-				relativeUrl = global.nconf.get('relative_url') || '';
-
-			postContent = postContent.replace(/(@\b[\w\d\-_]+\b)/g, function(match) {
-				return '<a class="plugin-mentions-a" href="' + relativeUrl + '/users/' + match.slice(1) + '">' + match + '</a>';
+var	async = require('async'),
+	Mentions = {
+		exists: function(slug, callback) {
+			RDB.get('userslug:' + slug + ':uid', function(err, uid) {
+				callback(!!uid);
 			});
+		},
+		addMentions: function(postContent, callback) {
+			var	_self = this,
+				regex = /(@\b[\w\d\-_]+\b)/g,
+				relativeUrl = global.nconf.get('relative_url') || '',
+				matches = postContent.match(/(@\b[\w\d\-_]+\b)/g);;
 
-			callback(null, postContent);
+			if (matches) {
+				async.filter(matches, function(match, next) {
+					var	userslug = match.slice(1);
+					_self.exists(userslug, next);
+				}, function(matches) {
+					matches.forEach(function(match) {
+						var	userslug = match.slice(1);
+						postContent = postContent.replace(match, '<a class="plugin-mentions-a" href="' + relativeUrl + '/users/' + userslug + '">' + match + '</a>');
+					});
+
+					callback(null, postContent);
+				});
+			} else callback(null, postContent);
+
+			// '<a class="plugin-mentions-a" href="' + relativeUrl + '/users/' + userslug + '">' + match + '</a>';
 		}
 	};
 
