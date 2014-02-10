ea1375fa66378d03c958f567a65befd275a63728
Julian Lam <julian@designcreateplay.com>
2014-02-10 14:24:19 -0500

    updated autocomplete lib and cleaned up a bit

--- static/autofill.js
+++ static/autofill.js
@@ -1,39 +1,35 @@
 $(document).ready(function() {
 	$('body').on('focus', '.composer .title, .composer .write', function(){
 		var composer = $('.composer .write');
-		if (composer.parent('.textcomplete-wrapper').length === 0) {
-			composer.textcomplete([{
-				match: /\B@([^\s\n]*)$/,
-				search: function (term, callback) {
-					socket.emit('user.search', term, function(err, userdata) {
-						if (!Array.isArray(userdata)) {
-							userdata = userdata.users;
-						}
 
-						if (err) {
-							return callback([]);
-						}
+		composer.textcomplete([{
+			match: /\B@([^\s\n]*)$/,
+			search: function (term, callback) {
+				socket.emit('user.search', term, function(err, userdata) {
+					if (!Array.isArray(userdata)) {
+						userdata = userdata.users;
+					}
 
-						callback(userdata.map(function(user) {
-							return user.username;
-						}).sort(function(a, b) {
-							return a.toLocaleLowerCase() > b.toLocaleLowerCase();
-						}));
-					});
-					// socket.emit('modules.composer.autofill', {'term': term}, function(err, data) {
-					//     callback(data.sort(function(a, b) {							// Sort alphabetically
-					//         return a.toLocaleLowerCase() > b.toLocaleLowerCase();
-					//     }));
-					// });
-				},
-				index: 1,
-				replace: function (mention) {
-					return '@' + mention.replace(/\s/g, '-') + ' ';
-				},
-				cache: true
-			}]);
+					if (err) {
+						return callback([]);
+					}
 
-			$('.textcomplete-wrapper').css('height', '100%').find('textarea').css('height', '100%');
-		}
+					callback(userdata.map(function(user) {
+						return user.username;
+					}).sort(function(a, b) {
+						return a.toLocaleLowerCase() > b.toLocaleLowerCase();
+					}));
+				});
+			},
+			index: 1,
+			replace: function (mention) {
+				return '@' + mention.replace(/\s/g, '-') + ' ';
+			},
+			cache: true
+		}]);
+
+		composer.attr('data-mentions', '1');
+
+		$('.textcomplete-wrapper').css('height', '100%').find('textarea').css('height', '100%');
 	});
 });
