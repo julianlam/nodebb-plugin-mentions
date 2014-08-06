d997904f2f8833b69b323d2ecd227379d3d09a89
barisusakli <barisusakli@gmail.com>
2014-08-06 17:32:56 -0400

    cache true

--- static/autofill.js
+++ static/autofill.js
@@ -89,7 +89,7 @@ $(document).ready(function() {
 				replace: function (mention) {
 					return '@' + collapseSpaces(mention) + ' ';
 				},
-				cache: false
+				cache: true
 			}]);
 
 			element.attr('data-mentions', '1');
