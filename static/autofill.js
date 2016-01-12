b9295705c6980479756391a2dbb5b03cfb248728
barisusakli <barisusakli@gmail.com>
2016-01-12 12:47:42 +0200

    https://github.com/NodeBB/NodeBB/issues/3842

--- static/autofill.js
+++ static/autofill.js
@@ -2,114 +2,104 @@
 /* globals socket, app, utils */
 
 
-(function(window) {
-	window.Mentions = {
-		groups: null,
-		addAutofill: function(element, localUserList) {
-			var subset;
+$(document).ready(function() {
+	var groupList = [];
+	var localUserList = [];
 
-			element.textcomplete([{
-				match: /\B@([^\s\n]*)?$/,
-				search: function (term, callback) {
-					var usernames;
-					if (!term) {
-						usernames = localUserList.concat(window.Mentions.groups).filter(function(value, index, array) {
-							return array.indexOf(value) === index && value !== app.user.username;
-						}).sort(function(a, b) {
-							return a.toLocaleLowerCase() > b.toLocaleLowerCase();
-						});
-						return callback(usernames);
+	$(window).on('composer:autocomplete:init', function(ev, data) {
+		localUserList = loadDomUsers();
+
+		if (!groupList.length) {
+			loadGroupList();
+		}
+
+		var subset;
+		var strategy = {
+			match: /\B@([^\s\n]*)?$/,
+			search: function (term, callback) {
+				var usernames;
+				if (!term) {
+					usernames = localUserList.concat(groupList).filter(function(value, index, array) {
+						return array.indexOf(value) === index && value !== app.user.username;
+					}).sort(function(a, b) {
+						return a.toLocaleLowerCase() > b.toLocaleLowerCase();
+					});
+					return callback(usernames);
+				}
+
+				socket.emit('user.search', {query: term}, function(err, userdata) {
+					if (err) {
+						return callback([]);
 					}
 
-					socket.emit('user.search', {query: term}, function(err, userdata) {
-						if (err) {
-							return callback([]);
-						}
-
-						usernames = userdata.users.map(function(user) {
-							return user.username;
-						});
-
-						subset = localUserList.concat(window.Mentions.groups).filter(function(username) {
-							return username.toLocaleLowerCase().indexOf(term.toLocaleLowerCase()) !== -1;
-						});
-
-						usernames = usernames.concat(subset).filter(function(value, index, array) {
-							return array.indexOf(value) === index;
-						});
-
-						subset = subset.map(function(name) {
-							return name.toLocaleLowerCase();
-						});
-
-						usernames.sort(function(a, b) {
-							if (subset.indexOf(a.toLocaleLowerCase()) !== -1 && subset.indexOf(b.toLocaleLowerCase()) === -1) {
-								return -1;
-							} else if (subset.indexOf(a.toLocaleLowerCase()) === -1 && subset.indexOf(b.toLocaleLowerCase()) !== -1) {
-								return 1;
-							} else {
-								return a.toLocaleLowerCase() > b.toLocaleLowerCase();
-							}
-						});
-
-						// Remove current user from suggestions
-						if (app.user.username && usernames.indexOf(app.user.username) !== -1) {
-							usernames.splice(usernames.indexOf(app.user.username), 1);
-						}
-
-						callback(usernames);
+					usernames = userdata.users.map(function(user) {
+						return user.username;
 					});
-				},
-				index: 1,
-				replace: function (mention) {
-					return '@' + utils.slugify(mention, true) + ' ';
-				},
-				cache: true
-			}], {
-				zIndex: 20000,
-				listPosition: function(position) {
-					this.$el.css(this._applyPlacement(position));
-					this.$el.css('position', 'absolute');
-					return this;
-				}
-			});
 
-			element.attr('data-mentions', '1');
-		}
-	};
+					subset = localUserList.concat(groupList).filter(function(username) {
+						return username.toLocaleLowerCase().indexOf(term.toLocaleLowerCase()) !== -1;
+					});
+
+					usernames = usernames.concat(subset).filter(function(value, index, array) {
+						return array.indexOf(value) === index;
+					});
+
+					subset = subset.map(function(name) {
+						return name.toLocaleLowerCase();
+					});
+
+					usernames.sort(function(a, b) {
+						if (subset.indexOf(a.toLocaleLowerCase()) !== -1 && subset.indexOf(b.toLocaleLowerCase()) === -1) {
+							return -1;
+						} else if (subset.indexOf(a.toLocaleLowerCase()) === -1 && subset.indexOf(b.toLocaleLowerCase()) !== -1) {
+							return 1;
+						} else {
+							return a.toLocaleLowerCase() > b.toLocaleLowerCase();
+						}
+					});
+
+					// Remove current user from suggestions
+					if (app.user.username && usernames.indexOf(app.user.username) !== -1) {
+						usernames.splice(usernames.indexOf(app.user.username), 1);
+					}
+
+					callback(usernames);
+				});
+			},
+			index: 1,
+			replace: function (mention) {
+				return '@' + utils.slugify(mention, true) + ' ';
+			},
+			cache: true
+		};
+
+		data.strategies.push(strategy);
+	});
 
 	$(window).on('action:composer.loaded', function(e, data) {
-		function onComposerLoaded() {
-			var composer = $('#cmp-uuid-' + data.post_uuid + ' .write'),
-				DOMusers = [];
-
-			// Retrieve DOM users
-			$('.posts [data-pid] .username-field').each(function(idx, el) {
-				var	username = el.getAttribute('data-username');
-				if (DOMusers.indexOf(username) === -1) {
-					DOMusers.push(username);
-				}
-			});
-
-			window.Mentions.addAutofill(composer, DOMusers);
-			$('.textcomplete-wrapper').css('height', '100%').find('textarea').css('height', '100%');
-		}
-
-		if (!window.Mentions.groups) {
-			return loadGroupList(onComposerLoaded);
-		}
-
-		onComposerLoaded();
+		var composer = $('#cmp-uuid-' + data.post_uuid + ' .write');
+		composer.attr('data-mentions', '1');
 	});
+
+	function loadDomUsers() {
+		var DOMusers = [];
+		$('.posts [data-pid] .username-field').each(function(idx, el) {
+			var	username = el.getAttribute('data-username');
+			if (DOMusers.indexOf(username) === -1) {
+				DOMusers.push(username);
+			}
+		});
+		return DOMusers;
+	}
 
 	function loadGroupList(callback) {
 		socket.emit('plugins.mentions.listGroups', function(err, groupNames) {
 			if (err) {
 				return app.alertError(err.message);
 			}
-			window.Mentions.groups = groupNames;
+			groupList = groupNames;
 			callback();
 		});
 	}
 
-})(window);
+});
