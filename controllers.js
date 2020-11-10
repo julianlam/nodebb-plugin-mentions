e5db7a86a2123a7257787bdafcd45330ab58051a
Tudor-Dan Ravoiu <tudor-dan.ravoiu@ubisoft.com>
2020-11-10 14:31:50 +0200

    remove console log

--- controllers.js
+++ controllers.js
@@ -5,9 +5,7 @@ var groups = require.main.require('./src/groups');
 var Controllers = module.exports;
 
 Controllers.renderAdminPage = function (req, res, next) {	
-	groups.getGroupsFromSet('groups:visible:createtime', 0, -1, function(err, groupData) {
-		console.log('err', err);
-		console.log('groupData', groupData);
+	groups.getGroupsFromSet('groups:visible:createtime', 0, -1, function(err, groupData) {		
 		if (err) {
 			return next(err);
 		}
