43be2fd9144406785c7da4b375176da9322c85fd
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-11-08 09:40:56 -0500

    feat: add progress to upgrade script

--- upgrades/mentions_delete_mentions_set_zset.js
+++ upgrades/mentions_delete_mentions_set_zset.js
@@ -7,12 +7,14 @@ module.exports = {
 	name: 'Delete mentions:sent:<pid> sorted sets',
 	timestamp: Date.UTC(2021, 10, 2),
 	method: async function () {
+        const { progress } = this;
         const nextPid = await db.getObjectField('global', 'nextPid');
         const allPids = [];
         for (let pid = 1; pid < nextPid; ++ pid) {
             allPids.push(pid);
         }
         await batch.processArray(allPids, async (pids) => {
+            progress.incr(pids.length);
             await db.deleteAll(pids.map(pid => `mentions:sent:${pid}`));
         }, {
             batch: 500,
