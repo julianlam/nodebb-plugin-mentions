julianlam/nodebb-plugin-mentions

commit d18bb45acbfe3e3169343cf29f6a33852eef9b1e

fix: wrap mention in a bi-directional isolate element to properly handle RTL userslugs.

library.js
@@ -489,7 +489,7 @@ Mentions.parseRaw = async (content, type = 'default') => {
 					if (type === 'markdown') {
 						str = `[${match}](${nconf.get('url')}${url})`;
 					} else {
-						str = `<a class="plugin-mentions-${mentionType} plugin-mentions-a" href="${url}">${match}</a>`;
+						str = `<a class="plugin-mentions-${mentionType} plugin-mentions-a" href="${url}">@<bdi>${match.slice(1)}</bdi></a>`;
 					}
 
 					return plain + str;

commit 165ae87ed75ba42ca3edfb89cf76310114975e44

fix: #314, regression caused by d18bb45acbfe3e3169343cf29f6a33852eef9b1e

library.js
@@ -473,7 +473,7 @@ Mentions.parseRaw = async (content, type = 'default') => {
 					// Again, cleaning up lookaround leftover bits
 					const atIndex = match.indexOf('@');
 					const plain = match.slice(0, atIndex);
-					match = match.slice(atIndex);
+					match = match.slice(atIndex + 1);
 					if (user && user.uid) {
 						switch (Mentions._settings.display) {
 							case 'fullname':
@@ -489,7 +489,7 @@ Mentions.parseRaw = async (content, type = 'default') => {
 					if (type === 'markdown') {
 						str = `[${match}](${nconf.get('url')}${url})`;
 					} else {
-						str = `<a class="plugin-mentions-${mentionType} plugin-mentions-a" href="${url}">@<bdi>${match.slice(1)}</bdi></a>`;
+						str = `<a class="plugin-mentions-${mentionType} plugin-mentions-a" href="${url}">${!Mentions._settings.display ? '@' : ''}<bdi>${match}</bdi></a>`;
 					}
 
 					return plain + str;