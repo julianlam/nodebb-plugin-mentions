443f69ba9382cbf5ebc5dd0517c6ee7c3613f158
Julian Lam <julian@nodebb.org>
2025-05-05 11:02:11 -0400

    fix: #315, missing relative_path on mentions url

--- library.js
+++ library.js
@@ -489,7 +489,7 @@ Mentions.parseRaw = async (content, type = 'default') => {
 					if (type === 'markdown') {
 						str = `[${match}](${nconf.get('url')}${url})`;
 					} else {
-						str = `<a class="plugin-mentions-${mentionType} plugin-mentions-a" href="${url}">${!Mentions._settings.display ? '@' : ''}<bdi>${match}</bdi></a>`;
+						str = `<a class="plugin-mentions-${mentionType} plugin-mentions-a" href="${nconf.get('relative_path')}${url}">${!Mentions._settings.display ? '@' : ''}<bdi>${match}</bdi></a>`;
 					}
 
 					return plain + str;
